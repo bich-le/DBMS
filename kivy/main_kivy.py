3cf2d89860564ba76affac9706b24116f2a99611
bich-le <lengocbich3805@gmail.com>
2025-05-04 00:24:51 +0700

    kivy

--- kivy/main_kivy.py
+++ kivy/main_kivy.py
@@ -3,35 +3,31 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, ObjectProperty
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+from kivy.uix.floatlayout import FloatLayout
+from kivy.graphics import Rectangle
+from kivy.graphics import Color
+from kivy.lang import Builder
+from kivy.uix.popup import Popup
 
-class MainScreen(BoxLayout):
-    my_text = StringProperty("Hello!")
-    
-    def change_text(self):
-        self.my_text = "You clicked the button"
-        
-class MyGrid(GridLayout):
-    def __init__(self,**kwargs):
-        super(MyGrid,self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text = "FName: "))
-        self.fname = TextInput(multiline= False)
-        self.add_widget(self.fname)
-        
-        self.add_widget(Label(text = "LName: "))
-        self.lname = TextInput(multiline= False)
-        self.add_widget(self.lname)
-        
-        self.add_widget(Label(text = "Email: "))
-        self.email = TextInput(multiline= False)
-        self.add_widget(self.email)
+class MainWindow(Screen):
+    pass
 
-class MyApp(App):
+class SecondWindow(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+kv = Builder.load_file("my.kv")
+
+class MyMainApp(App):
     def build(self):
-        return MyGrid()
+        return kv
 
 if __name__ == '__main__':
-    MyApp().run()
+    MyMainApp().run()
+    
