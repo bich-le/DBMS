eaa94bf065cc674bd58a506991b1a78bec6b1798
bich-le <lengocbich3805@gmail.com>
2025-05-04 10:30:43 +0700

    remove_tkinter

--- tkinter_sample/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-import tkinter as tk
-from tkinter import messagebox
-from tkinter_sample.customer_account import CustomerAccount
-from kivy.tkinter_sample.admin import Admin
-import csv
-import mysql.connector
