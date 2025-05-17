e6fe737c737384f0e62237f5af4f7ddbcbedf047
7hufofbun <11230592@st.neu.edu.vn>
2025-05-17 16:50:14 +0700

    Update 3. [MAIN] PROCEDURE UPDATE.sql

--- Database Main/3. [MAIN] PROCEDURE UPDATE.sql
+++ Database Main/3. [MAIN] PROCEDURE UPDATE.sql
@@ -1,3 +1,6 @@
+#################################################
+        -- UPDATE TRANSACTIONS --
+#################################################
 DELIMITER //
 
 CREATE PROCEDURE AddTransaction(
@@ -65,21 +68,88 @@ DELIMITER ;
 -- Khai báo biến để nhận kết quả
 SET @result = '';
 SET @error_code = '';
+#################################################
+        -- UPDATE CUSTOMER ACCOUNTS --
+#################################################
+CREATE PROCEDURE update_customer_account(
+    IN p_cus_account_id VARCHAR(17),
+    IN p_account_type_id VARCHAR(2),
+    IN p_balance BIGINT UNSIGNED,
+    IN p_interest_rate_id TINYINT,
+    IN p_transfer_limit INT UNSIGNED,
+    IN p_daily_transfer_limit BIGINT UNSIGNED,
+    IN p_deposit_amount BIGINT UNSIGNED,
+    IN p_deposit_date DATE,
+    IN p_maturity_date DATE,
+    OUT p_result VARCHAR(255),
+    OUT p_error_code VARCHAR(20)
+)
+update_proc: BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION 
+    BEGIN
+        SET p_result = 'Error: System error while updating account';
+        SET p_error_code = 'SYSTEM_ERROR';
+    END;
 
--- Gọi thủ tục
-CALL AddTransaction(
-    'TRF',                          -- p_trans_type_id
-    'DTNBS250000001',              -- p_cus_account_id
-    'DTNBC240000001',              -- p_related_cus_account_id
-    1501,                          -- p_trans_amount
-    'Debit',                       -- p_direction
-    'Successful',                  -- p_trans_status
-    NULL,                          -- p_trans_error_code
-    '2025-05-17 16:00:00',         -- p_trans_time
-    '2025-05-17 16:00:00',         -- p_last_updated
-    @result,                       -- OUT p_result
-    @error_code                    -- OUT p_error_code
-);
-select * from transactions;
--- Xem kết quả
-SELECT @result, @error_code;
+    SET p_result = '';
+    SET p_error_code = '';
+
+    IF p_account_type_id = 'S' THEN
+        UPDATE SAVING_ACCOUNTS
+        SET saving_acc_balance = IFNULL(p_balance, saving_acc_balance),
+            interest_rate_id = IFNULL(p_interest_rate_id, interest_rate_id)
+        WHERE cus_account_id = p_cus_account_id;
+
+    ELSEIF p_account_type_id = 'C' THEN
+        UPDATE CHECK_ACCOUNTS
+        SET check_acc_balance = IFNULL(p_balance, check_acc_balance),
+            interest_rate_id = IFNULL(p_interest_rate_id, interest_rate_id),
+            transfer_limit = IFNULL(p_transfer_limit, transfer_limit),
+            daily_transfer_limit = IFNULL(p_daily_transfer_limit, daily_transfer_limit)
+        WHERE cus_account_id = p_cus_account_id;
+
+    ELSEIF p_account_type_id = 'F' THEN
+        UPDATE FIXED_DEPOSIT_ACCOUNTS
+        SET deposit_amount = IFNULL(p_deposit_amount, deposit_amount),
+            interest_rate_id = IFNULL(p_interest_rate_id, interest_rate_id),
+            deposit_date = IFNULL(p_deposit_date, deposit_date),
+            maturity_date = IFNULL(p_maturity_date, maturity_date)
+        WHERE cus_account_id = p_cus_account_id;
+
+    ELSE
+        SET p_result = 'Error: Invalid account type';
+        SET p_error_code = 'INVALID_ACCOUNT_TYPE';
+        LEAVE update_proc;
+    END IF;
+
+    IF ROW_COUNT() > 0 THEN
+        SET p_result = 'Update successful';
+        SET p_error_code = 'SUCCESS';
+    ELSE
+        SET p_result = 'No rows updated - check cus_account_id or all values NULL';
+        SET p_error_code = 'NO_UPDATE';
+    END IF;
+END;
+//
+DELIMITER ;
+
+-- Tạo biến đầu ra
+SET @result = '';
+SET @error_code = '';
+-- -- Gọi thủ tục
+-- CALL AddTransaction(
+--     'TRF',                          -- p_trans_type_id
+--     'DTNBS250000001',              -- p_cus_account_id
+--     'DTNBC240000001',              -- p_related_cus_account_id
+--     1501,                          -- p_trans_amount
+--     'Debit',                       -- p_direction
+--     'Successful',                  -- p_trans_status
+--     NULL,                          -- p_trans_error_code
+--     '2025-05-17 16:00:00',         -- p_trans_time
+--     '2025-05-17 16:00:00',         -- p_last_updated
+--     @result,                       -- OUT p_result
+--     @error_code                    -- OUT p_error_code
+-- );
+-- select * from transactions;
+-- -- Xem kết quả
+-- SELECT @result, @error_code;
